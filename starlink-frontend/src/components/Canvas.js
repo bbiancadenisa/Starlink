dd02f80d60302fe7a6fb2aae3f6824b4c8217fa2
Bianca Denisa <biancadenisa93@yahoo.com>
2021-09-06 15:17:12 +0300

    feat: added stars

--- starlink-frontend/src/components/Canvas.js
+++ starlink-frontend/src/components/Canvas.js
@@ -13,17 +13,49 @@ function Canvas({ latLng }) {
       }
     }
     if (context !== null) {
+      let width = window.innerWidth;
+      let height = window.innerHeight - 50;
+      context.fillStyle = "red";
+      context.fillRect(width / 2, height / 2, 10, 10);
+      context.fillRect(500, 500, 20, 20);
       latLng.map((coords) => {
         let x, y;
+        let spikes = 5;
+        let innerRadius = 1;
+        let outerRadius = 5;
         if (coords.latitude < 0) {
-          x = ((45 - Math.abs(coords.latitude)) * 1200) / 180;
-        } else x = ((45 + coords.latitude) * 1200) / 180;
+          x = ((45 - Math.abs(coords.latitude)) * width) / 180;
+        } else x = ((45 + coords.latitude) * width) / 180;
 
         if (coords.longitude < 0) {
-          y = ((69 - Math.abs(coords.longitude)) * 600) / 180;
-        } else y = ((69 + coords.longitude) * 600) / 180;
+          y = ((69 - Math.abs(coords.longitude)) * height) / 180;
+        } else y = ((69 + coords.longitude) * height) / 180;
+        var rot = (Math.PI / 2) * 3;
+        var step = Math.PI / spikes;
+        let cx = x;
+        let cy = y;
 
-        context.fillRect(x, y, 1, 1);
+        context.strokeSyle = "#000";
+        context.beginPath();
+        context.moveTo(cx, cy - outerRadius);
+        for (let i = 0; i < spikes; i++) {
+          x = cx + Math.cos(rot) * outerRadius;
+          y = cy + Math.sin(rot) * outerRadius;
+          context.lineTo(x, y);
+          rot += step;
+
+          x = cx + Math.cos(rot) * innerRadius;
+          y = cy + Math.sin(rot) * innerRadius;
+          context.lineTo(x, y);
+          rot += step;
+        }
+        context.lineTo(cx, cy - outerRadius);
+        context.closePath();
+        context.lineWidth = 1;
+        context.strokeStyle = "white";
+        context.stroke();
+        context.fillStyle = "#f7f8fa";
+        context.fill();
         return 0;
       });
     }
@@ -31,7 +63,11 @@ function Canvas({ latLng }) {
 
   return (
     <div className="canvas">
-      <canvas ref={canvasRef} width={1204} height={604} />
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth - 50}
+        height={window.innerHeight - 50}
+      />
     </div>
   );
 }
